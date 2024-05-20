05ee2091ddfa44fd1c634c3bddea5ef08a70886b
MikeLindvang <mike@netscribepro.com>
2024-05-20 14:12:54 +0200

    Fix environment variable configuration

--- pages/api/auth/me.js
+++ pages/api/auth/me.js
@@ -4,6 +4,7 @@ export default async function me(req, res) {
   try {
     await auth0.handleProfile(req, res);
   } catch (error) {
-    res.status(error.status || 400).end(error.message);
+    console.error('ME ERROR: ', error);
+    res.status(error.status || 500).end(error.message);
   }
 }
--- pages/api/check-env.js
+++ pages/api/check-env.js
@@ -1,14 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
+export default function handler(req, res) {
+  res.json({
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     AUTH0_SECRET: process.env.AUTH0_SECRET,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
     AUTH0_ISSUER: process.env.AUTH0_ISSUER,
-  },
-};
-
-export default nextConfig;
+  });
+}
